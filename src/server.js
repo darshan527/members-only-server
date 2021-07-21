3e7d19f2e3e3757c6eed1ca549471af1028a9022
darshan527 <darshan.hosamani6@gmail.com>
2021-07-21 14:51:19 +0530

    server file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import * as admin from 'firebase-admin';
+import credentials from './credentials.json';
+
+import { db } from './db';
 import { routes } from './routes';
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+})
+
 const app = express();
 
-
+app.use(bodyParser.json());
 
 routes.forEach(route => {
     app[route.method](route.path, route.handler);
 });
 
-app.listen(8080, () => {
-    console.log("Server is listening on port 8080");
-});
+const start = async () => {
+    await db.connect('mongodb://localhost:2717');
+    app.listen(8080);
+    console.log("Listening on port 8080");
+}
+
+start();
