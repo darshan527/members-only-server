0ec65de6b261286d6082fd14e98c7c6515e75f18
darshan527 <darshan.hosamani6@gmail.com>
2021-08-04 23:52:00 +0530

    Added getUserGroupsRoute

--- src/routes/getUserGroupsRoute.js
+++ src/routes/getUserGroupsRoute.js
@@ -1,8 +1,22 @@
+import * as admin from 'firebase-admin'
+import { getUserGroups } from '../db'
+
+
 export const getUserGroupsRoute = {
     method: 'get',
     path: '/users/:id/groups',
     handler: async (req, res) => {
+        const token = req.headers.authtoken
+        const userId = req.params.id
 
+        const user = await admin.auth().verifyIdToken(token)
+        console.log(user, userId)
+        if (user.user_id !== userId) {
+            return res.status(401).json({ message: "Not Authorized!" })
+        }
+
+        const userGroups = getUserGroups(userId)
+        res.status(200).json(userGroups)
 
     }
 }
