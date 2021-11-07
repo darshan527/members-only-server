974243ab7a0b6d9420b84c678e16ac8713a04078
darshan527 <darshan.hosamani6@gmail.com>
2021-11-07 20:09:48 +0530

    Implemented createGroupRoute

--- src/routes/createGroupRoute.js
+++ src/routes/createGroupRoute.js
@@ -1,7 +1,20 @@
+import * as admin from 'firebase-admin';
+import { createGroup } from '../db'
+
 export const createGroupRoute = {
     method: 'post',
     path: '/groups',
     handler: async (req, res) => {
+        const token = req.headers.authtoken;
+        const { name } = req.body;
 
+        if (!token) {
+            res.status(401).json({ message: 'Must be authenticated to create new groups.' });
+        }
+
+        const user = await admin.auth().verifyIdToken(token)
+        const newGroupId = await createGroup(name, user.user_id);
+
+        res.status(200).json({ newGroupId })
     }
 }
